59f3a046c6f52bba8dea05cda30f02969d1b465e
bryanhancco <bhanccoco@unsa.edu.pe>
2022-04-28 23:10:42 -0500

    suma

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,7 @@
 class Calculator {
-  int add(int a, int b){ return 0; }
+  int add(int a, int b){ 
+	return a+b;
+  }
   int sub(int a, int b){ return 0; }
   int mul(int a, int b){
     return a*b;
