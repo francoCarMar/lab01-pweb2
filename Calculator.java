5603aeda6bebde3869602c6014d737b3c65deae0
Eberth Wilfredo Azurin Zuñiga <eazurin@unsa.edu.pe>
2022-04-28 23:14:51 -0500

    Creando la division

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,8 @@ class Calculator {
   int mul(int a, int b){
     return a*b;
   }
-  int div(int a, int b){ return 0; }
+  int div(int a, int b){ 
+    return a/b;
+  }
   int mod(int a, int b){ return 0; }
 }
